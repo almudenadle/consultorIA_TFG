26857a7430dcc7591aaba08bbd700822d78350d0
almudenadelaescalera <almudenadelaescalera@gmail.com>
2026-05-27 21:34:06 +0200

    Cambiado puerto

--- frontend/src/enviroment/enviroment.ts
+++ frontend/src/enviroment/enviroment.ts
@@ -1,4 +1,5 @@
 export const enviroment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api'
+   // apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'https://consultoria-tfg.onrender.com'
 };
